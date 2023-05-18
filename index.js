59f18999a30b92b7489c424986816f3b03a625dc
Joaby <joabyoliveira2004@gmail.com>
2023-05-18 00:06:45 -0300

    deploy

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ server.use(middlewares)
 server.use(router)
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
